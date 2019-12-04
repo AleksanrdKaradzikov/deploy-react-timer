b7784cf4f8f9a253468338340f926e448f19f96a
AleksandrKaradzikov <karadzikovaleksandr@gmail.com>
2019-12-04 15:05:13 +0300

    update App component be func

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Tabs, Icon } from 'antd';
@@ -7,38 +6,36 @@ import Countdown from './components/countdown';
 import 'antd/dist/antd.css';
 import './style/style.css';
 
-class App extends React.Component {
-  render() {
-    const { TabPane } = Tabs;
-    return (
-      <div className="tab-container">
-        <Tabs defaultActiveKey="2">
-          <TabPane
-            tab={
-              <span>
-                <Icon type="clock-circle" />
-                Секундомер
-              </span>
-            }
-            key="1"
-          >
-            <Timer />
-          </TabPane>
-          <TabPane
-            tab={
-              <span>
-                <Icon type="history" />
-                Таймер обратного отсчета
-              </span>
-            }
-            key="2"
-          >
-            <Countdown />
-          </TabPane>
-        </Tabs>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const { TabPane } = Tabs;
+  return (
+    <div className="tab-container">
+      <Tabs defaultActiveKey="2">
+        <TabPane
+          tab={
+            <span>
+              <Icon type="clock-circle" />
+              Секундомер
+            </span>
+          }
+          key="1"
+        >
+          <Timer />
+        </TabPane>
+        <TabPane
+          tab={
+            <span>
+              <Icon type="history" />
+              Таймер обратного отсчета
+            </span>
+          }
+          key="2"
+        >
+          <Countdown />
+        </TabPane>
+      </Tabs>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
